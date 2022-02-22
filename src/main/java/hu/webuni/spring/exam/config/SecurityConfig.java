a937b3cdeb98c3629ca740978043cd73d5bc987c
Kovács Sámuel <sunikovacs@gmail.com>
2022-02-22 20:35:32 +0100

    basic auth works

---
+++ src/main/java/hu/webuni/spring/exam/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package hu.webuni.spring.exam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user").authorities("user").password(passwordEncoder().encode("pass"))
+                .and()
+                .withUser("admin").authorities("user", "admin").password(passwordEncoder().encode("pass"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/addresses/**").hasAnyAuthority("admin")
+                .antMatchers(HttpMethod.GET, "/api/addresses/**").hasAnyAuthority("user", "admin")
+                .anyRequest().authenticated();
+    }
+}
---
+++ src/main/java/hu/webuni/spring/exam/controller/JwtLoginController.java
@@ -0,0 +1,16 @@
+package hu.webuni.spring.exam.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JwtLoginController {
+
+    /*
+    @PostMapping("/api/login")
+    public String login(LoginDto loginDto) {
+        //
+    }
+
+     */
+}
